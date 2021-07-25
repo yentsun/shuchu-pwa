c75cc1f3e8c6e31ab1a73855fabb1b058fae3d1a
Maksim Korinets <mkorinets@gmail.com>
2021-07-25 14:37:08 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './Card/Card';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
