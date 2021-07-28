64bca3defe95fa366e1514c9d5179132f856ee4b
Maksim Korinets <mkorinets@gmail.com>
2021-07-28 16:20:55 +0700

    Add title and flavor text

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import Card from './Card/Card';
 import photo1 from './Card/31_19 06 21.jpg';
 import photo2 from './Card/IMG_9013.jpg';
 import photo3 from './Card/DSC_3142.JPG';
+import photo4 from './Card/7080328517736425992-account_id=1.jpg';
+import photo5 from './Card/12011115_1479684472339077_7101187587758133558_n.jpg';
+import photo6 from './Card/Изображение 471.jpg';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card color="#a793acff" image={{ x: -15, y: -30, width: 260, xlinkHref: photo1 }} />
-        <Card color="black" image={{ x: -20, y: -20, width: 260, xlinkHref: photo2 }} />
-        <Card color="grey" image={{ x: -15, y: -30, width: 260, xlinkHref: photo3 }} />
+        <Card title="before wedding" flavor="moments before this beauty belongs to a man"
+              color="#a793acff" image={{ x: -15, y: -30, width: 260, xlinkHref: photo1 }} />
+        <Card title="the pose" flavor="boss at day, playful cat at night" color="black" image={{ x: -20, y: -20, width: 260, xlinkHref: photo2 }} />
+        <Card title="street queen" flavor="she dominates until you conquer her" color="grey" image={{ x: -15, y: -30, width: 260, xlinkHref: photo3 }} />
+        <Card title="vika" flavor="" color="#5A932D48" image={{ x: -30, y: -100, width: 350, xlinkHref: photo4 }} />
+        <Card title="dasha" flavor="mouthwatering devil" color="#93593948" image={{ x: -140, y: -50, xlinkHref: photo5 }} />
+        <Card title="julia" flavor="the fitch outfit" color="grey" image={{ x: -15, y: -30, width: 600, xlinkHref: photo6 }} />
       </header>
     </div>
   );
--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -5,7 +5,7 @@ import './card.css';
 const WIDTH = 228;
 const HEIGHT = 332;
 
-export default function Card({ image, color }) {
+export default function Card({ image, color, title, flavor }) {
     return (<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"
                  width={ WIDTH } height={ HEIGHT }>
 
@@ -15,14 +15,21 @@ export default function Card({ image, color }) {
             </clipPath>
 
             <radialGradient id={ color } cx="50%" cy="50%" r="50%" >
-                <stop offset="30%" style={{ 'stop-color': color,'stop-opacity':1.00}} />
+                <stop offset="30%" style={{ 'stop-color': color,'stop-opacity':1.00 }} />
                 <stop offset="100%" style={{ 'stop-color': color, 'stop-opacity':0.00 }} />
             </radialGradient>
         </defs>
 
-        <g clipPath="url(#base)">
+        <g clipPath="url(#base)" width={ WIDTH } >
             <image {...image }  />
-            <rect x={ 0 } y={ HEIGHT - HEIGHT / 3 } width={ WIDTH } height={ HEIGHT / 3 } fill={ `url(#${color})` } />
+            <rect x={ 0 } y={ HEIGHT - HEIGHT / 3 } width="100%" height={ HEIGHT / 3 } fill={ `url(#${color})` } />
+            <text textAnchor="middle" x="50%" y={ HEIGHT - HEIGHT / 5.2 } className="title" fillOpacity="0.9">{ title }</text>
+            <path strokeWidth={ 0.4 } opacity={ 0.8 } stroke="white" d={ `m 10,280 h 208 z` } />
+            <foreignObject x="30" y="270" width="180" height="50" opacity="0.8">
+                <p xmlns="http://www.w3.org/1999/xhtml" className="flavor">
+                    { flavor }
+                </p>
+            </foreignObject>
         </g>
 
 
