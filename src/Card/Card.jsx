e9e166bf493d0ed33d3b91b2548c07aa4f11d9d5
Maksim Korinets <mkorinets@gmail.com>
2021-07-29 15:09:14 +0700

    Make data url work with SVG

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -5,6 +5,17 @@ import './card.css';
 const WIDTH = 228;
 const HEIGHT = 332;
 
+const titleStyle =  {
+    font: `30px serif`,
+    fill: `white`
+}
+
+const flavorStyle = {
+    font: `italic 15px serif`,
+    color: `white`,
+    lineHeight: `1em`
+}
+
 export default function Card({ image, color, title, flavor }) {
     return (<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"
                  width={ WIDTH } height={ HEIGHT }>
@@ -15,18 +26,18 @@ export default function Card({ image, color, title, flavor }) {
             </clipPath>
 
             <radialGradient id={ color } cx="50%" cy="50%" r="50%" >
-                <stop offset="30%" style={{ 'stop-color': color,'stop-opacity':1.00 }} />
-                <stop offset="100%" style={{ 'stop-color': color, 'stop-opacity':0.00 }} />
+                <stop offset="30%" style={{ stopColor: color,stopOpacity: 1.00 }} />
+                <stop offset="100%" style={{ stopColor: color, stopOpacity: 0.00 }} />
             </radialGradient>
         </defs>
 
         <g clipPath="url(#base)" width={ WIDTH } >
             <image {...image }  />
             <rect x={ 0 } y={ HEIGHT - HEIGHT / 3 } width="100%" height={ HEIGHT / 3 } fill={ `url(#${color})` } />
-            <text textAnchor="middle" x="50%" y={ HEIGHT - HEIGHT / 5.2 } className="title" fillOpacity="0.9">{ title }</text>
+            <text textAnchor="middle" x="50%" y={ HEIGHT - HEIGHT / 5.2 } style={ titleStyle } fillOpacity="0.9">{ title }</text>
             <path strokeWidth={ 0.4 } opacity={ 0.8 } stroke="white" d={ `m 10,280 h 208 z` } />
             <foreignObject x="30" y="270" width="180" height="50" opacity="0.8">
-                <p xmlns="http://www.w3.org/1999/xhtml" className="flavor">
+                <p xmlns="http://www.w3.org/1999/xhtml" style={ flavorStyle }>
                     { flavor }
                 </p>
             </foreignObject>
